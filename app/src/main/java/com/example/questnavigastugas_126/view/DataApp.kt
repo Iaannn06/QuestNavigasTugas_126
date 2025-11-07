9c885c6de1990e4cc4c9ac3f0a16e8c2375a9ec7
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 15:23:57 +0700

    menambahkan import untuk DataApp

---
+++ app/src/main/java/com/example/questnavigastugas_126/view/DataApp.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigastugas_126.view
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_126.viewmodel.FormViewModel
+
+
