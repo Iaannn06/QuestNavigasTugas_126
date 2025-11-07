9a954dd92d199ebd6ed8bc8359881a9a2c24a2c9
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:26:08 +0700

    menambahkan horizontalAlignment dan verticalArrangement

--- app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
@@ -1,11 +1,19 @@
 package com.example.questnavigastugas_126.view
 
-import android.graphics.Color
+
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.R
+
+
 
 val PurpleBg = Color(0xFFF3E5F5)
 val PurpleButton = Color(0xFFF3E5F5)
@@ -19,5 +27,9 @@ fun WelcomeScreen(
             .fillMaxSize()
             .background(PurpleBg)
             .padding(dimensionResource(R.dimen.padding_medium))
-    )
+        horinzontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+    }
 }
