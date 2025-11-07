c321e128ee090851282681069388b813e22f77ad
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:20:24 +0700

    menambahkan modifier untuk WelcomeScreen

--- app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
@@ -1,7 +1,11 @@
 package com.example.questnavigastugas_126.view
 
 import android.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 val PurpleBg = Color(0xFFF3E5F5)
 val PurpleButton = Color(0xFFF3E5F5)
@@ -10,5 +14,10 @@ val PurpleButton = Color(0xFFF3E5F5)
 fun WelcomeScreen(
     onSubmitClick: () -> Unit
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PurpleBg)
+            .padding(dimensionResource(R.dimen.padding_medium))
+    )
 }
