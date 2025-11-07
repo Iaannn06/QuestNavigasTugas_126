db455e55a76d94f0dd41434a92f0ec90a67ffe67
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 15:33:47 +0700

    penyesuaian pada MainActivity.kt

--- app/src/main/java/com/example/questnavigastugas_126/MainActivity.kt
+++ app/src/main/java/com/example/questnavigastugas_126/MainActivity.kt
@@ -1,47 +1,26 @@
-package com.example.questnavigastugas_126
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.questnavigastugas_126.view.DataApp
 import com.example.questnavigastugas_126.ui.theme.QuestNavigasTugas_126Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             QuestNavigasTugas_126Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DataApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasTugas_126Theme {
-        Greeting("Android")
     }
 }
