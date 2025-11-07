32ac60f6139dea560df9e3d8b8bfaf53ad18d103
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:49:51 +0700

    perbaikan pada FormViewModel serta menambahkan import pada FormIsian.kt dengan warna

---
+++ app/src/main/java/com/example/questnavigastugas_126/viewmodel/FormViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.questnavigastugas_126.viewmodel
+
+package com.example.questnavigastugas_126.view
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+data class FormUiState(
+    data class FormUiState(
+val nama: String = "",
+val jenisKelamin: String = "Laki-Laki",
+val statusPerkawinan: String = "Lajang",
+val alamat: String = ""
+)
+
+class FormViewModel : ViewModel() {
+    val uiState by mutableStateOf(FormUiState())
+        private set
+
+    fun SetNama(nama: String) {
+        uiState = uiState.copy(nama = nama)
+    }
+
+    fun setJenisKelamin(jenisKelamin: String) {
+        uiState = uiState.copy(jenisKelamin = jenisKelamin)
+    }
+
+    fun setStatusPerkawinan(status: String) {
+        uiState = uiState.copy(statusPerkawinan = status)
+    }
+
+    fun setAlamat(alamat: String) {
+        uiState = uiState.copy(alamat = alamat)
+    }
+
+
+}
+)
