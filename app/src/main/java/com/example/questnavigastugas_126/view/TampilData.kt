2cfaba31623338d82ed796f71b33b8d23f180fa9
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 15:18:32 +0700

    menambahkan import serta nilai untuk warna

---
+++ app/src/main/java/com/example/questnavigastugas_126/view/TampilData.kt
@@ -0,0 +1,21 @@
+package com.example.questnavigastugas_126.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas_126.R
+import com.example.questnavigastugas_126.viewmodel.FormViewModel
+
+val PurpleBgLight = Color(0xFFF3E5F5)
+val PurpleButtonDark = Color(0xFF8E24AA)
+val PurpleHeader = Color(0xFFAB47BC)
+val PurpleCard = Color(0xFFE1BEE7)
