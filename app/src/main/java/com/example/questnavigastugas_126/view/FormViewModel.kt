2e0f8efd25733f98bdb4cfad40cfb061a71e0035
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:07:10 +0700

    menambahkan isi dari data class berisi FormUiState untuk semua isian form

--- app/src/main/java/com/example/questnavigastugas_126/view/FormViewModel.kt
+++ app/src/main/java/com/example/questnavigastugas_126/view/FormViewModel.kt
@@ -2,6 +2,9 @@ package com.example.questnavigastugas_126.view
 
 data class FormUiState(
     data class FormUiState(
-
+        val nama: String = "",
+        val jenisKelamin: String = "Laki-Laki",
+        val statusPerkawinan: String = "Lajang",
+        val alamat: String = ""
     )
 )
