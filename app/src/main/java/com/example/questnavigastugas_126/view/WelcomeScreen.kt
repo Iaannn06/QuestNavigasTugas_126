f2e2f262e235c1f8fc15646c8090296a47c31b58
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:30:39 +0700

    menambahkan import dan modifier text

--- app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
@@ -1,20 +1,25 @@
 package com.example.questnavigastugas_126.view
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.Alignment
-import androidx.compose.runtime.R
-
-
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.QuestNavigasTugas_126.R
 
 val PurpleBg = Color(0xFFF3E5F5)
 val PurpleButton = Color(0xFFF3E5F5)
@@ -27,11 +32,15 @@ fun WelcomeScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(PurpleBg)
-            .padding(dimensionResource(R.dimen.padding_medium))
+            .padding(dimensionResource(R.dimen.padding_medium)),
         horinzontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         Text(
+            text = stringResource(R.string.welcome_title),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = purpleButton
 
         )
     }
