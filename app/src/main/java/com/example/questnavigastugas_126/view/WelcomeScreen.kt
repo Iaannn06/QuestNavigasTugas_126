61e1a29451109cdbb0a19bad3c3f056b52726133
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-07 14:17:47 +0700

    menambahkan function untuk WelcomeScreen

--- app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_126/view/WelcomeScreen.kt
@@ -1,7 +1,14 @@
 package com.example.questnavigastugas_126.view
 
 import android.graphics.Color
+import androidx.compose.runtime.Composable
 
 val PurpleBg = Color(0xFFF3E5F5)
 val PurpleButton = Color(0xFFF3E5F5)
 
+@Composable
+fun WelcomeScreen(
+    onSubmitClick: () -> Unit
+) {
+
+}
